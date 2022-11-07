03e797f5d10c2364f2c64e712e61af3603f34b0b
mitoma99 <114859605+mitoma99@users.noreply.github.com>
2022-11-07 11:30:54 +0000

    [finish2]task5

--- bookers2-debug/app/models/user.rb
+++ bookers2-debug/app/models/user.rb
@@ -9,17 +9,7 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
 
-#  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-#  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
 
-#  has_many :followings, through: :relationships, source: :followed
-#  has_many :followers, through: :reverse_of_relationships, source: :follower
-
-  #has_many :relationships
-  #has_many :followings, through: :relationships, source: :followed
-  #has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'followed_id'
-  #has_many :followers, through: :reverse_of_relationships, source: :follower
-  
   # フォローをした、されたの関係
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
@@ -28,21 +18,8 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
 
-  # def follow(other_user)
-  #   unless self == other_user
-  #     self.relationships.find_or_create_by(followed_id: other_user.id)
-  #   end
-  # end
 
-  # def unfollow(other_user)
-  #   relationship = self.relationships.find_by(followed_id: other_user.id)
-  #   relationship.destroy if relationship
-  # end
 
-  # def following?(other_user)
-  #   self.followings.include?(other_user)
-  # end
-  
   # フォローしたときの処理
   def follow(user_id)
     relationships.create(followed_id: user_id)
@@ -71,19 +48,19 @@ class User < ApplicationRecord
   end
 
 
-  #def follow(user_id)
-
-   # relationships.create(followed_id: user_id)
-  #end
-
-  #def unfollow(user_id)
-   # relationships.find_by(followed_id: user_id).destroy
-  #end
-
-  #def following?(user)
-   # followings.include?(user)
-  #end
-
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?", "#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?", "%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?", "%#{word}%")
+    else
+      @user = User.all
+    end
+  end
 
 end
 
--- bookers2-debug/config/routes.rb
+++ bookers2-debug/config/routes.rb
@@ -5,27 +5,21 @@ Rails.application.routes.draw do
 
   root to: "homes#top"
   get "home/about"=>"homes#about"
-  
+
   devise_for :users
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
-  end 
-  
+  end
 
-   #resource :relationships, only: [:create, :destroy]
-   
-   
+
    resources :users do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
+
+  get "search" => "searches#search"
   
-  
-  #resources :users, only: [:index,:show,:edit,:update]
-  
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- 
-end 
+end
