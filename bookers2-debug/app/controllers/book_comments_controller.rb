14bc27c09bdd130b128d668cd942f1125cb25efe
mitoma99 <114859605+mitoma99@users.noreply.github.com>
2022-11-10 10:35:14 +0000

    [finish2]task6

--- bookers2-debug/app/controllers/book_comments_controller.rb
+++ bookers2-debug/app/controllers/book_comments_controller.rb
@@ -1,25 +1,28 @@
 class BookCommentsController < ApplicationController
-  
+
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
+    @book_comment = BookComment.new
+    comment.book_id = @book.id
     if comment.save
-      redirect_to book_path(book.id)
+      render :book_comments
     else
-    redirect_to request.referer
-    end 
+      redirect_to request.referer
+    end
   end
-  
+
   def destroy
+    @book = Book.find(params[:book_id])
     BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
-  end 
-  
+    @book_comment = BookComment.new
+    render :book_comments
+  end
+
   private
-  
+
   def book_comment_params
     params.require(:book_comment).permit(:comment)
-  end 
-  
+  end
+
 end
